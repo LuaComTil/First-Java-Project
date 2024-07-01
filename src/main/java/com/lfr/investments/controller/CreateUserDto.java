aa3f190065edece772a264c99ce8b44fdc9911f2
LuaComTil <luadefreitasramos@gmail.com>
2024-07-01 13:02:01 -0300

    added create user feature

---
+++ src/main/java/com/lfr/investments/controller/CreateUserDto.java
@@ -0,0 +1,4 @@
+package com.lfr.investments.controller;
+
+public record CreateUserDto(String username, String email, String password) {
+}
--- src/main/java/com/lfr/investments/controller/UserController.java
+++ src/main/java/com/lfr/investments/controller/UserController.java
@@ -1,20 +1,30 @@
 package com.lfr.investments.controller;
 
 import com.lfr.investments.entity.User;
+import com.lfr.investments.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody String body) {
-        return  null;
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto CreateUserDto) {
+        var userId = userService.createUser(CreateUserDto);
+        return ResponseEntity.created(URI.create("/v1/users/" + userId)).build();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
-        return  null;
+        return null;
     }
 }
